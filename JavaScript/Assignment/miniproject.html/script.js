9d43d74388d9f4cfa5e859865152fa2a453d27c6
farheenqazi-Thinkitive <farheen.qazi@thinkitive.com>
2025-11-04 15:17:56 +0530

    changes done

--- JavaScript/Assignment/miniproject.html/script.js
+++ JavaScript/Assignment/miniproject.html/script.js
@@ -1,48 +1,51 @@
  
- 
- let URL ="https://playground.mockoon.com/customers";
- let URL2="https://playground.mockoon.com/photos";
-//   const get = async()=>{
-//     const res= await fetch(URL);
-//     return res.json()
-    
+ let URL = "https://playground.mockoon.com/customers";
+let URL2 = "https://playground.mockoon.com/photos";
 
-//   }
-// const res = await fetch(URL)
-//     .then(res => res.json())
-//     .then(data => data) 
+document.getElementById("button").addEventListener("click", loadData);
+document.getElementById("button2").addEventListener("click", loadImages);
 
-let dataGlobal
-const coutmerData = async () => {
+let dataGlobal = [];
+
+const customerData = async () => {
   const res = await fetch(URL);
-  const data = await res.json();
+  return res.json();
+};
+
+async function loadData() {
+  const data = await customerData();
   dataGlobal = data;
-  return data;
-};
 
-let fun=async () => {
-  await coutmerData();
-  console.log(dataGlobal);
+  const tableBody = document.getElementById("customers-body");
+  tableBody.innerHTML = ""; // Clear old rows
+
+  dataGlobal.forEach((customer) => {
+    const row = `
+      <tr>
+        <td>${customer.name}</td>
+        <td>${customer.email}</td>
+        <td>${customer.phone}</td>
+      </tr>`;
+    tableBody.innerHTML += row;
+  });
 }
-fun();
-let dataGlobal2
-const coutmerImage = async () => {
+async function loadImages() {
   const res = await fetch(URL2);
-  const data = await res.json();
-  dataGlobal2 = data;
-  return data;
-};
+  const photos = await res.json();
 
-cument.getElementById("button").addEventListener("click", async () => {
-  await customerData();
-  displayCustomerData();
-});
+  const imgTableBody = document.getElementById("photos-body");
+  imgTableBody.innerHTML = "";
 
-let funs=async () => {
-  await coutmerImage();
-  console.log(dataGlobal2);
+  photos.forEach((photo) => {
+    const row = `
+      <tr>
+        <td><img src="${photo.url}" width="100"></td>
+        <td>${photo.title || "No Title"}</td>
+        <td>${photo.id}</td>
+      </tr>`;
+    imgTableBody.innerHTML += row;
+  });
 }
-funs();
 
 
 
